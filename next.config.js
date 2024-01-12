b78a64070070d1f124beb63617c700adc5005e28
jrrene7 <jrrene7@gmail.com>
2024-01-11 20:41:00 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-150434945",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig
